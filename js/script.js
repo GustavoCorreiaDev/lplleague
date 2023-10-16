c68bdcd399cab0fc19b5b1f823daea5b44a41081
Gustavo Correia <gustavocorreias@live.com>
2023-10-16 14:27:35 -0300

    add all darkMode

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
+    
     navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
+
 }
 
 window.onscroll = () =>{
@@ -12,6 +13,18 @@ window.onscroll = () =>{
 const changeThemeBtn = document.querySelector('#change-theme')
 
 changeThemeBtn.addEventListener('change', function() {
+    
     let containerDiv = document.querySelector('.container');
     containerDiv.classList.toggle('dark')
+
+    let categoryDivs = document.querySelectorAll('.categoryCard');
+    categoryDivs.forEach(function(categoryDiv) {
+        categoryDiv.classList.toggle('darkCategoryCard');
+    });
+
+    let titleDivs = document.querySelectorAll('.mainCategoryCardTitle');
+    titleDivs.forEach(function(titleDiv) {
+        titleDiv.classList.toggle('darkMainCategoryCardTitle')
+    })
+    
 })
